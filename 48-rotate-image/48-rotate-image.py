1381d5498fdd07242179e57e894569afadb263e7
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-05-11 23:26:35 +0530

    Time: 64 ms (18.23%), Space: 14 MB (30.12%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -7,12 +7,12 @@ class Solution:
         for i in range(0,x):
             for j in range(i, n-i-1):
                 temp1=matrix[i][j]
-                temp2=matrix[j][n-i-1]
                 temp3=matrix[n-i-1][n-j-1]
-                temp4=matrix[n-j-1][i]
-                matrix[i][j]=temp4
+                
+                matrix[i][j]=matrix[n-j-1][i]
+                
+                matrix[n-i-1][n-j-1]=matrix[j][n-i-1]
                 matrix[j][n-i-1]=temp1
-                matrix[n-i-1][n-j-1]=temp2
                 matrix[n-j-1][i]=temp3
 
 
