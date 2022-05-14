ca298a5de74c7bbf4f148deb29f71f40fb8595b6
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-05-14 12:30:13 +0530

    Time: 173 ms (33.68%), Space: 14.2 MB (89.41%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,30 +1,21 @@
-def my_enumerate(sequence):
 
-    n = 0
-    my_list=[]
-    for elem in sequence:
-        my_list.append([elem,n])
-        n += 1
-    return my_list
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         n=len(mat)
         m=len(mat[0])
-        arr=[]
+        my_arr=[]
         for i in range(n):
             count=0
             for j in range(m):
                 if mat[i][j]==1:
                     count+=1
-                else: 
-                    break
-            arr.append(count)
-        my_arr=my_enumerate(arr)
+            my_arr.append([count,i])
+
 
         my_arr.sort()
-        arr.clear()
+        output=[]
         for i in range(k):
-            arr.append(my_arr[i][1])
-        return arr
-        
+            output.append(my_arr[i][1])
+
+        return output
