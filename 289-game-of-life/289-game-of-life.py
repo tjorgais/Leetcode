ef4accd1188c59a5bb8b9b119ce11073874687bc
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-05-18 22:17:22 +0530

    Time: 52 ms (33.03%), Space: 13.9 MB (47.34%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -3,6 +3,10 @@ class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         n=len(board)
         m=len(board[0])
+        for i in range(n):
+            for j in range(m):
+                if board[i][j]==0:
+                    board[i][j]=2
         matrix=[[board[i][j] for j in range(m)]for i in range(n)]
         neighbors=[(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1)]
         for i in range(n):
@@ -11,15 +15,20 @@ class Solution:
                 for neighbor in neighbors:
                     r=i+neighbor[0]
                     c=j+neighbor[1]
-                    if (r>=0 and r<n) and (c>=0 and c<m) and matrix[r][c]==1:
+
+                    if (r>=0 and r<n) and (c>=0 and c<m) and abs(matrix[r][c])==1:
                         count+=1
                 if matrix[i][j]==1 and (count<2 or count>3):
+                    board[i][j]=-1
+                if abs(matrix[i][j])==2 and (count==3):
+                    board[i][j]=-2
+        for i in range(n):
+            for j in range(m):
+                if board[i][j]==-1:
                     board[i][j]=0
-                if matrix[i][j]==0 and (count==3):
+                elif board[i][j]==2:
+                    board[i][j]=0
+                elif board[i][j]==-2:
                     board[i][j]=1
-
-
-
-
 
         return 
