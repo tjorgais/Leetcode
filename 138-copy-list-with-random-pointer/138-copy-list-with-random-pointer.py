6351999bad292e7ed5e00a9a8cf3864a37e844d0
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-05-26 16:08:53 +0530

    Time: 86 ms (5.36%), Space: 15 MB (21.21%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,36 +9,36 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        return deepcopy(head)
-#         temp=head
-#         temp2=None
-#         if head==None:
-#             return temp2
-#         else:
-            
-#             while(temp!=None):
-#                 temp2=Node(temp.val)
-#                 temp2.next=temp.next
-#                 temp.next=temp2
-#                 temp=temp.next.next
-#             temp=head
-#             while(temp!=None):
-#                 if temp.random:
-#                     temp.next.random=temp.random.next
-#                 temp=temp.next.next
-            
-#             temp=head
 
-#             temp2=head.next
-#             temp3=temp2
-#             while(temp.next.next!=None):
-#                 temp.next=temp.next.next
-#                 temp2.next=temp2.next.next
-#                 temp=temp.next
-#                 temp2=temp2.next
-#             temp.next=None
-#             temp2.next=None
-#             return temp3
+        temp=head
+        temp2=None
+        if head==None:
+            return temp2
+        else:
+            
+            while(temp!=None):
+                temp2=Node(temp.val)
+                temp2.next=temp.next
+                temp.next=temp2
+                temp=temp.next.next
+            temp=head
+            while(temp!=None):
+                if temp.random:
+                    temp.next.random=temp.random.next
+                temp=temp.next.next
+            
+            temp=head
+
+            temp2=head.next
+            temp3=temp2
+            while(temp.next.next!=None):
+                temp.next=temp.next.next
+                temp2.next=temp2.next.next
+                temp=temp.next
+                temp2=temp2.next
+            temp.next=None
+            temp2.next=None
+            return temp3
             
                 
                 
