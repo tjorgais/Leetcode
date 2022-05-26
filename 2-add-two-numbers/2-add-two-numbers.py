306b7f29795ab5a2cf74762c658bff083bfaa295
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-05-26 19:03:16 +0530

    Time: 124 ms (18.14%), Space: 14 MB (10.53%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -14,11 +14,7 @@ def add_list(head, n):
         while(temp.next!=None):
             temp=temp.next
         temp.next=node
-    return head
-    
-        
-    
-    
+    return head 
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
