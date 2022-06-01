a1f5efaa0a8819683963869c5d288bec46bcd69e
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-06-01 22:08:36 +0530

    Time: 91 ms (49.90%), Space: 18 MB (50.07%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,57 +1,57 @@
-class MinStack:
-
-    def __init__(self):
-        self.stack=[]
-    def push(self, val: int) -> None:
-        #lst = list()
-        self.stack.append(val)
-    def pop(self) -> None:
-        self.stack.pop()
-
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        return min(self.stack)
-
-
-
-
 # class MinStack:
 
 #     def __init__(self):
 #         self.stack=[]
-#         self.min_stack=[]
-#         self.min=float('inf')
-        
-
 #     def push(self, val: int) -> None:
+#         #lst = list()
 #         self.stack.append(val)
-#         if len(self.stack)==0:
-#             self.min_stack.append(val)
-#             self.min=val
-#         else:
-#             if val>=self.min: 
-#                 self.min_stack.append(val)
-#             else:
-#                 self.min_stack.append(2*val-self.min)
-#                 self.min=val
-        
-
 #     def pop(self) -> None:
 #         self.stack.pop()
-#         if self.min_stack[-1]<self.min:
-#             self.min=2*self.min-self.min_stack[-1]
-#             self.min_stack.pop()
-#         else:
-#             self.min_stack.pop()
-            
+
 #     def top(self) -> int:
 #         return self.stack[-1]
-        
 
 #     def getMin(self) -> int:
-#         return self.min
+#         return min(self.stack)
+
+
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.min_stack=[]
+        self.min=float('inf')
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.stack)==0:
+            self.min_stack.append(val)
+            self.min=val
+        else:
+            if val>=self.min: 
+                self.min_stack.append(val)
+            else:
+                self.min_stack.append(2*val-self.min)
+                self.min=val
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.min_stack[-1]<self.min:
+            self.min=2*self.min-self.min_stack[-1]
+            self.min_stack.pop()
+        else:
+            self.min_stack.pop()
+            
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min
         
         
 
