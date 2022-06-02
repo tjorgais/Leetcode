99e6fa0e99e70d48178011d885f4f25fc238d417
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-06-02 23:14:40 +0530

    Time: 696 ms (88.06%), Space: 19.1 MB (68.90%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,27 +1,43 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        start=0
-        end=1
+        s,start,diff=0,0,0
         n=len(gas)
-        if n==1:
-            if gas[0]-cost[0]<0:
-                return -1
-            else:
-                return 0
-        else:
-            curr_petrol=gas[start]-cost[start]
-            while end!=start or curr_petrol<0:
-                while curr_petrol<0 and start!=end:
-                    curr_petrol=curr_petrol-(gas[start]-cost[start])
-                    start=(start+1)%n
-                    if start==0:
-                        return -1
-                curr_petrol=curr_petrol+gas[end]-cost[end]
-                end=(end+1)%n
+        for i in range(n):
+            s=s+gas[i]-cost[i]
+            if(s<0):
+                start=i+1
+                diff+=s
+                s=0
+
+        if s+diff>=0:
             return start
         
+        else:
+            return -1
+        
+        #********Solution 2******************
+        # start=0
+        # end=1
+        # n=len(gas)
+        # if n==1:
+        #     if gas[0]-cost[0]<0:
+        #         return -1
+        #     else:
+        #         return 0
+        # else:
+        #     curr_petrol=gas[start]-cost[start]
+        #     while end!=start or curr_petrol<0:
+        #         while curr_petrol<0 and start!=end:
+        #             curr_petrol=curr_petrol-(gas[start]-cost[start])
+        #             start=(start+1)%n
+        #             if start==0:
+        #                 return -1
+        #         curr_petrol=curr_petrol+gas[end]-cost[end]
+        #         end=(end+1)%n
+        #     return start
         
         
+#****************BruteForce************  
 #         n=len(gas)
 #         sum1=sum(gas)
 #         sum2=sum(cost)
