b26a3d9ef89d1a57febcffea00feb3d44c16e1a6
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-06-28 19:16:46 +0530

    Time: 78 ms (72.34%), Space: 16.2 MB (20.48%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,41 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def __init__(self):
+        self.func={}
+    def connectRight(self,root,row, col ):
+        if root==None:
+            return 
+        arr=self.func.get(row,[])
+        arr.append([root, col])
+        self.func[row]=arr
+        self.connectRight(root.left,row+1,col-1)
+        self.connectRight(root.right, row+1,col+1)
+        return
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root==None:
+            return None
+        self.connectRight(root,0,0)
+        for key in sorted(self.func.keys()):
+            sorted(self.func[key], key= lambda x: (x[1]))
+            n=len(self.func[key])
+            for i in range(n-1):
+                self.func[key][i][0].next=self.func[key][i+1][0]
+            self.func[key][n-1][0].next=None
+        return root
+            
+                
+            
+            
+        
+        
+        
+        
