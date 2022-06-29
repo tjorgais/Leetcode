b6c4664d857bbb9c65bbe456fdc3a6c071f806fa
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-06-29 18:42:31 +0530

    Time: 63 ms (75.05%), Space: 18 MB (44.27%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -5,21 +5,43 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.list1=[]
-    def inorder(self, root):
+    def Deletemin(self, root):
         if root==None:
-            return 
-        self.inorder(root.left)
-        self.list1.append(root.val)
-        self.inorder(root.right)
-        return
-    
+            return None
+        if root.left:
+            root.left=self.Deletemin(root.left)
+            return root
+        return root.right
+    def findmin(self, root):
+        if root==None:
+            return None
+        if root.left:
+            return self.findmin(root.left)
+        return root.val
+        
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         if root==None:
             return None
-        self.inorder(root)
-        return self.list1[k-1]
+        i=1
+        while(i!=k):
+            root=self.Deletemin(root)
+            i+=1
+        return self.findmin(root)
+#     def __init__(self):
+#         self.list1=[]
+#     def inorder(self, root):
+#         if root==None:
+#             return 
+#         self.inorder(root.left)
+#         self.list1.append(root.val)
+#         self.inorder(root.right)
+#         return
+    
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         if root==None:
+#             return None
+#         self.inorder(root)
+#         return self.list1[k-1]
         
         
         
