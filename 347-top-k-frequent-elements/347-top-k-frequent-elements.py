e34ded14f71999ef5a2d4a54069ff184a109299b
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-04 23:48:55 +0530

    Time: 174 ms (37.89%), Space: 18.8 MB (46.88%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,8 @@
+from heapq import heappush, heappop
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        
         n=len(nums)
         freq = {}
         for i in range(n):
@@ -7,13 +10,34 @@ class Solution:
                 freq[nums[i]] += 1
             else:
                 freq[nums[i]] = 1
-        a=[0]*(len(freq))
-        print(a)
-        j=0
-        for keys in freq:
+        heap=[]
+        heapq.heapify(heap)
+        for key in freq.keys():
+            heappush(heap,[-freq[key],key])
+        #print(heap)
+        output=[]
+        while(k):
+            x,y=heappop(heap)
+            output.append(y)
+            k-=1
+        return output
+        
+        
+        
+#         n=len(nums)
+#         freq = {}
+#         for i in range(n):
+#             if nums[i] in freq:
+#                 freq[nums[i]] += 1
+#             else:
+#                 freq[nums[i]] = 1
+#         a=[0]*(len(freq))
+#         print(a)
+#         j=0
+#         for keys in freq:
 
-            a[j]=[keys, freq[keys]]
-            j+=1
-        a = sorted(a, key=lambda x: x[1], reverse=True)
+#             a[j]=[keys, freq[keys]]
+#             j+=1
+#         a = sorted(a, key=lambda x: x[1], reverse=True)
 
-        return [a[i][0] for i in range(k)]
+#         return [a[i][0] for i in range(k)]
