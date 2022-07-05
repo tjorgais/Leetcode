5f0442226b9cb531a1820dffa9e2e8e6da59376a
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-05 16:20:31 +0530

    Time: 36 ms (89.75%), Space: 14 MB (27.50%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+from collections import Counter
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        
+        i=0
+        func=Counter(s)
+        if max(func.values())>(len(s)+1)/2:
+            return ""
+        heap=[]
+        heapq.heapify(heap)
+        for key in func.keys():
+            heappush(heap,[-func[key],key])
+        prev_freq=0
+        prev_char='#'
+        output=[]
+        while heap:
+            freq,c=heappop(heap)
+            output.append(c)
+            if prev_freq<0:
+                heappush(heap,[prev_freq,prev_char ])
+            prev_freq=freq+1
+            prev_char=c
+        return ''.join(output)
