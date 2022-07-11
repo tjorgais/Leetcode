fac0e37a46f940eb7c59a6850d3d5cfb0648c977
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-11 22:20:13 +0530

    Time: 223 ms (26.59%), Space: 19.2 MB (13.49%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,18 +1,20 @@
 class Solution:
-    def check_previous(self, stack):
-        if not stack:
-            return 0
-        count=1
-        i=-1
-        temp=[]
-        x=stack.pop()
-        temp.append(x)
-        while(stack and stack[-1]==x ):
-            temp.append(stack.pop())
-            count+=1
-        while(temp):
-            stack.append(temp.pop())
-        return count
+    # def check_previous(self, stack):
+    #     if not stack:
+    #         return 0
+    #     count=1
+    #     i=-1
+    #     temp=[]
+    #     x=stack.pop()
+    #     temp.append(x)
+    #     while(stack and stack[-1]==x ):
+    #         temp.append(stack.pop())
+    #         count+=1
+    #     while(temp):
+    #         stack.append(temp.pop())
+    #     return count
+
+    
     def removeDuplicates(self, s: str, k: int) -> str:
         stack=[]
         n=len(s)
@@ -21,30 +23,23 @@ class Solution:
 
         while(i!=n):
 
-            if not stack:
-                stack.append(s[i])
-                count=1
-                i+=1
-                continue
-            x=stack[-1]
-            if x!=s[i]:
-                stack.append(s[i])
-                count=1
+
+            if not stack or stack[-1][0]!=s[i]:
+                stack.append([s[i],1])
             else:
-
-
-                if count==k-1:
-                    j=0
-                    removed=True
-                    while(j!=k-1):
-                        stack.pop()
-                        j+=1
-                    count=self.check_previous(stack)
-
-                else:
-                    stack.append(s[i])
-                    count+=1
+                x,y=stack.pop()
+                stack.append([x,y+1])
+                if stack[-1][1]==k:
+                    stack.pop()
             i+=1
-                
-        return "".join(stack)
+        output=[]
+        print(stack)
+        while(stack):
+            x,y=stack.pop()
+            while(y):
+                output.append(x)
+                y-=1
+        
+            
+        return "".join(output[::-1])
         
