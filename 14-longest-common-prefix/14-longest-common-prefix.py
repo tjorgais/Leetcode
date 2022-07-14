5335ba2420f464ff509c0d113d12760927b9e76d
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-14 19:07:40 +0530

    Time: 32 ms (96.48%), Space: 13.8 MB (88.15%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,34 +1,51 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
         n=len(strs)
-        
-
-        match=strs[0]
-        
-        
+        minlen=len(strs[0])
         for i in range(1,n):
-            x=strs[i]
-            m=len(x)
-            j=1
-            if m<len(match):
+            if len(strs[i])<minlen:
+                minlen=len(strs[i])
+        output=''
+        for i in range(minlen):
+            curr=strs[0][i]
+            for j in range(1,n):
+                if strs[j][i]!=curr:
+                    return output
+            output+=curr
+        return output
+                    
                 
-                while(j!=m+1):
-                    if match[:j]!=x[:j]:
-                        match=match[:j-1]
-                        break
+        
+        
+        
+        #Time complexity of O(N*L) where L is length of the string in worse case
+#         n=len(strs) 
+#         match=strs[0]
+#         for i in range(1,n):
+#             x=strs[i]
+#             m=len(x)
+#             j=1
+#             if m<len(match):
+                
+#                 while(j!=m+1):
+#                     if match[:j]!=x[:j]:
+#                         match=match[:j-1]
+#                         break
                     
                         
-                    j+=1
-                if j==m+1:
-                    match=x[:m]
+#                     j+=1
+#                 if j==m+1:
+#                     match=x[:m]
                 
-            else:
-                while(j!=len(match)+1):
-                    if match[:j]!=x[:j]:
-                        match=match[:j-1]
-                        break
-                    j+=1
-        return match
+#             else:
+#                 while(j!=len(match)+1):
+#                     if match[:j]!=x[:j]:
+#                         match=match[:j-1]
+#                         break
+#                     j+=1
+#         return match
                     
                         
         
