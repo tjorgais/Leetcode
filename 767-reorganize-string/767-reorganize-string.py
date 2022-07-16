c63f1cb97458f72becb65e93a38b8260c4447f09
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-16 18:31:03 +0530

    Time: 47 ms (64.79%), Space: 13.8 MB (98.69%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -2,24 +2,37 @@ from heapq import heappush, heappop
 from collections import Counter
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        n=len(s)
+        a=[]
+        for count, c in sorted((s.count(c),c) for c in set(s)):
+            if count>(n+1)/2:
+                return ""
+            a.extend(c*count)
+        result=[None]*n
+        result[::2]=a[n//2:]
+        result[1::2]=a[:n//2]
+        return ''.join(result)
         
-        i=0
-        func=Counter(s)
-        if max(func.values())>(len(s)+1)/2:
-            return ""
-        heap=[]
-        heapq.heapify(heap)
-        for key in func.keys():
-            heappush(heap,[-func[key],key])
-        prev_freq=0
-        prev_char='#'
-        output=[]
-        while heap:
-            freq,c=heappop(heap)
-            output.append(c)
-            if prev_freq<0:
-                heappush(heap,[prev_freq,prev_char ])
-            prev_freq=freq+1
-            prev_char=c
+            
+            
+        #Using heap
+#         i=0
+#         func=Counter(s)
+#         if max(func.values())>(len(s)+1)/2:
+#             return ""
+#         heap=[]
+#         heapq.heapify(he    ap)
+#         for key in func.keys():
+#             heappush(heap,[-func[key],key])
+#         prev_freq=0
+#         prev_char='#'
+#         output=[]
+#         while heap:
+#             freq,c=heappop(heap)
+#             output.append(c)
+#             if prev_freq<0:
+#                 heappush(heap,[prev_freq,prev_char ])
+#             prev_freq=freq+1
+#             prev_char=c
         
-        return ''.join(output)
+#         return ''.join(output)
