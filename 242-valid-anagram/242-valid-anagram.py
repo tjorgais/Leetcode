1949571d5e01cbbcc2a8d87d33131424b27d690a
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-20 19:20:21 +0530

    Time: 63 ms (71.86%), Space: 14.4 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,26 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        func1=Counter(s)
+        func2=Counter(t)
+        if func1==func2:
+            return True
+        else:
             return False
-        func=Counter(s)
-        for l in t:
-            if l in func and func[l]!=0:
-                func[l]-=1
-            else:
-                return False
-        return True
+        
+        
+        ##using dictionary O(n)
+        
+        
+        # if len(s)!=len(t):
+        #     return False
+        # func=Counter(s)
+        # for l in t:
+        #     if l in func and func[l]!=0:
+        #         func[l]-=1
+        #     else:
+        #         return False
+        # return True
+    
+    ##Approach 2: O(nlogn) sort both sting and compare if they are same or not.
         
