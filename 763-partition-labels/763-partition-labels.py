958b8ccbeea0f8fb77eba0392af3547e38d75bca
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-07-29 18:28:03 +0530

    Time: 54 ms (68.75%), Space: 13.9 MB (18.49%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,35 +1,48 @@
 from collections import Counter
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        if not s:
-            return 0
-        n=len(s)
-        start=0
-        c=s[0]
-        j=n-1
-        func1=Counter(s)
+        last_occurances={c: i for i,c in enumerate(s)}
+        j,start=0,0
         output=[]
-        while(start<n):
-            while j>start:
-                if c==s[j]:
-                    break
-                j-=1
-            func2=Counter(s[start:j+1])
-            for key in func2:
-                if func1[key]!=func2[key]:
-                    c=key
-                    break
-                    
-            else:
-                output.append(len(s[start:j+1]))
-                if j==n-1:
-                    break
-                start=j+1
-                c==s[start]
-                j=n-1
-                continue
-            j=n-1
+        for i, c in enumerate(s):
+            j=max(j, last_occurances[c])
+            if i==j:
+                output.append(j-start+1)
+                start=i+1
         return output
+                
+        
+        
+        #My approach
+#         if not s:
+#             return 0
+#         n=len(s)
+#         start=0
+#         c=s[0]
+#         j=n-1
+#         func1=Counter(s)
+#         output=[]
+#         while(start<n):
+#             while j>start:
+#                 if c==s[j]:
+#                     break
+#                 j-=1
+#             func2=Counter(s[start:j+1])
+#             for key in func2:
+#                 if func1[key]!=func2[key]:
+#                     c=key
+#                     break
+                    
+#             else:
+#                 output.append(len(s[start:j+1]))
+#                 if j==n-1:
+#                     break
+#                 start=j+1
+#                 c==s[start]
+#                 j=n-1
+#                 continue
+#             j=n-1
+#         return output
                 
                 
                     
