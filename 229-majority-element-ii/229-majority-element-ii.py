8baf1c6c29c000270fcb173898a4669e4f608a89
tjorgais <42938890+tjorgais@users.noreply.github.com>
2022-09-24 23:45:45 +0530

    Time: 271 ms (16.71%), Space: 15.4 MB (67.58%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -4,17 +4,18 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         count= Counter(nums)
         n=len(nums)
-        x=abs(n/3)
-        # majority=[k for k, v in sorted(count.items(), key=lambda item: item[1]) if v>x] 
-        temp_list=[[-v, k] for k, v in count.items()]
-        heapq.heapify(temp_list)
-
-        nums.clear()
-        while len(temp_list)>0:
-            a,b=heappop(temp_list)
     
-            if -(a)>x:
-                nums.append(b)
-            else:
-                break
-        return nums
+        return [x[0] for x in count.items() if x[1]>abs(n/3)]
+#         # majority=[k for k, v in sorted(count.items(), key=lambda item: item[1]) if v>x] 
+#         temp_list=[[-v, k] for k, v in count.items()]
+#         heapq.heapify(temp_list)
+
+#         nums.clear()
+#         while len(temp_list)>0:
+#             a,b=heappop(temp_list)
+    
+#             if -(a)>x:
+#                 nums.append(b)
+#             else:
+#                 break
+#         return nums
